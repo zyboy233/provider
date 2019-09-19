d7360a23ddded818c9aabf2de9e4ab1fef481681
zyboy233 <451253127@qq.com>
2019-09-19 15:02:41 +0800

    add venv

--- myApp/urls.py
+++ myApp/urls.py
@@ -2,16 +2,16 @@ from django.urls import path
 from myApp import views
 
 urlpatterns = [
-    path(r'', views.home),
-    path(r'api/echarts_phones1',views.echarts_phones1),
-    path(r'api/echarts_phones2',views.echarts_phones2),
-    path(r'chinamobile/',views.chinamobile),
-    path(r'chinamobile/<int:id>/',views.chinamobile_id),
-    path(r'chinatelecom/',views.chinatelecom),
-    path(r'chinatelecom/<int:id>/',views.chinatelecom_id),
-    path(r'chinaunicom/',views.chinaunicom),
-    path(r'chinaunicom/<int:id>/',views.chinaunicom_id),
-    path(r'chinaunicom_pkg/',views.chinaunicom_pkg),
-    path(r'chinaunicom_pkg/<str:type_id>/',views.chinaunicom_pkg_id),
-    path(r'chinamobile_pkg/',views.chinamobile_pkg)
+    path(r'', views.home,name='home'),
+    path(r'api/echarts_phones1',views.echarts_phones1,name='api_p1'),
+    path(r'api/echarts_phones2',views.echarts_phones2,name='api_p2'),
+    path(r'chinamobile/',views.chinamobile,name='mobile'),
+    path(r'chinamobile/<int:id>/',views.chinamobile_id,name='mobile_id'),
+    path(r'chinatelecom/',views.chinatelecom,name='telecom'),
+    path(r'chinatelecom/<int:id>/',views.chinatelecom_id,name='telecom_id'),
+    path(r'chinaunicom/',views.chinaunicom,name='unicom'),
+    path(r'chinaunicom/<int:id>/',views.chinaunicom_id, name='unicom_id'),
+    path(r'chinaunicom_pkg/',views.chinaunicom_pkg,name='unicom_pkg'),
+    path(r'chinaunicom_pkg/<str:type_id>/',views.chinaunicom_pkg_id,name='unicom_pkg_id'),
+    path(r'chinamobile_pkg/',views.chinamobile_pkg, name='mobile_pkg')
 ]
